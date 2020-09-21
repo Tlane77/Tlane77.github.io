fc2e9fa413ac3cb32996b8385e184a36eb9977b9
Tasha Lane <designtmg77@gmail.com>
2020-09-21 18:08:09 -0500

    added pictures

--- Portfolio/js/init.js
+++ Portfolio/js/init.js
@@ -469,7 +469,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Software Developer", "Web Developer", "UI/UX Designer", "Team Mom"],
+			strings: ["Full Stack Developer", "Web Developer", "UI/UX Designer", "Team Mom"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
